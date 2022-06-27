47425ede4093d3c80daa0afb26885454edfee6ae
Rixaar <106077800+Rixaar@users.noreply.github.com>
2022-06-27 22:39:12 +0200

    Code IA

---
+++ Assets/Sounds/VOIXIAAAA/IADialogue.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IADialogue : MonoBehaviour
+{
+    public bool IATrigger = false;
+    public GameObject IA;
+    public Animator AnimTable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IA.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnIA();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            IATrigger = true;
+        }
+    }
+
+    void SpawnIA()
+    {
+        if (IATrigger == true)
+        {
+            IA.SetActive(true);
+            AnimTable.SetBool("TriggerTable", true);
+        }
+
+    }
+}
