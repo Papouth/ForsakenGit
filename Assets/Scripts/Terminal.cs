079d622fb1b32f6ccb94c161ca2ea5a33cf414c2
Rixaar <106077800+Rixaar@users.noreply.github.com>
2022-06-27 23:31:26 +0200

    pure hasard

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -6,22 +6,64 @@ public class Terminal : Interactable
 {
     public bool isUsed = false;
     private AudioSource dataAudio;
+    public bool Desactiver = false; //Richard
+    public IADialogue IAD; //Richard
+    public float DesactiverTPS = 0; //Richard
+    public bool desactiverterminal = false;
 
 
     public void Start()
     {
         dataAudio = GetComponent<AudioSource>();
+       // IAD = GetComponent<IADialogue>(); //Richard 
+    }
+
+    public void Update() //Richard
+    {
+        if(IAD.TerminalActive == true)
+        {
+            Desactiver = true;
+            DesactiverTPS = DesactiverTPS + Time.deltaTime;
+
+        }
+
+        if(DesactiverTPS >= 13)
+        {
+            DesactiverTPS = 13;
+            IAD.TerminalActive = false;
+            Desactiver = false;
+        }
+
+        if(Desactiver == true)
+        {
+            desactiverterminal = true;
+        }
+        else
+        {
+            desactiverterminal = false;
+        }
     }
 
     public override void Interact(bool value)
     {
-        // -- Display ou non mon UI de terminal
+       /* // -- Display ou non mon UI de terminal
         UIManager.uiManager?.DisplayTerminalMenu(value);
 
         StatesPlayer.statesPlayer.isInteractTerminal = value; // -- Si mon joueur interargis avec un terminal
         StatesPlayer.statesPlayer.canMoove = !value; // -- Si mon joueur peut ou non bouger
         StatesPlayer.statesPlayer.canLookAround = !value; // -- Si mon joueur peut regarder autour de lui ou non
-        isUsed = value;
+        isUsed = value; */
+
+        if (desactiverterminal == false) //Richard
+        {
+            // -- Display ou non mon UI de terminal
+            UIManager.uiManager?.DisplayTerminalMenu(value);
+
+            StatesPlayer.statesPlayer.isInteractTerminal = value; // -- Si mon joueur interargis avec un terminal
+            StatesPlayer.statesPlayer.canMoove = !value; // -- Si mon joueur peut ou non bouger
+            StatesPlayer.statesPlayer.canLookAround = !value; // -- Si mon joueur peut regarder autour de lui ou non
+            isUsed = value;
+        }
     }
 
     public void PlayData()
